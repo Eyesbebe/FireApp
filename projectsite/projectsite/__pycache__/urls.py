83f00af7f75ebd3fe8b009005710aed0a77d4368
Aizel Cantado <201920253@psu.palawan.edu.ph>
2024-05-31 20:34:36 +0800

    modified urls.py

---
+++ projectsite/projectsite/__pycache__/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity
+from fire import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', HomePageView.as_view(), name='home'),
+    path('ChartJs', ChartView.as_view(), name='ChartJs'),
+    path('chart/', PieCountbySeverity, name='chart'),
+    path('lineChart/', LineCountbyMonth, name='chart'),
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiBarChart/', multipleBarbySeverity, name='chart'),
+    path('stations', views.map_station, name='map-station'),
+     path('fire_incidents_map/', views.fire_incidents_map, name='fire_incidents_map'),
+
+]
